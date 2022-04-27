985daf06c806b4cf6d5a9898be21eeed5ee00c21
Willy <willy@coinerella.com>
2022-04-27 16:57:10 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Foundation Website coming soon.
+          <br />Stay tuned.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
