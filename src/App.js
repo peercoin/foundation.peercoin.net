493d234fa8372d4f11e96963b1fe421867a39c02
Willy <willy@coinerella.com>
2022-05-03 15:09:08 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.scss';
 import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Header />
       <Footer />
-    </div>
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div className='header'>
+            <img className='header__logo' src="/img/logo-foundation-white.png" alt="Peercoin Foundatin Logo" />
+        </div >
+    )
+}
